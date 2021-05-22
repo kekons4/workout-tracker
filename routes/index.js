bec97c3c91e986ab823140963acea87400f14c31
Keon Pourboghrat <kekons4@gmail.com>
2021-05-22 14:36:22 -0400

    potential fix to routes

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ const apiRoutes = require('./api');
 const statsRoutes = require('./statsRoute');
 const exerciseRoutes = require('./exerciseRoutes');
 
-router.use('/api', apiRoutes);
+router.use('aqueous-brushlands-30253.herokuapp.com/api', apiRoutes);
 router.use('/stats', statsRoutes);
 router.use('/exercise', exerciseRoutes);
 
